363d0601d8f62579df4b46e458cd6da0297866eb
nbrsx <123748085+nbrsx@users.noreply.github.com>
2024-11-09 06:17:58 +0000

    Update index.js
    token

--- files/index.js
+++ files/index.js
@@ -7,7 +7,7 @@ const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo.07yyyr.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiOGM0YTNiM2EzYjU2YjU4N2FlNjI2MTQyN2Y0MGNmNDYiLCJ0IjoiMWJiMmNiZmMtZDU5Yy00ZjgwLTgxYmUtNTkwYTE0ODRhNDc4IiwicyI6Ik9EQmhNakZpWWpRdE9HWTVOaTAwWXpjMkxUbG1PREF0TUdVMVlqUmpZekE1TnpNNCJ9';
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiOGM0YTNiM2EzYjU2YjU4N2FlNjI2MTQyN2Y0MGNmNDYiLCJ0IjoiNThmNmJiMzYtYjIxMS00YjM5LWE5MWYtNjQ3MDE0NjNkYzE5IiwicyI6Ik5qZzFOelUyWmpBdE1EQTVNUzAwWmprd0xXRmhNek10TjJNMVpqQmxPRFkxTlRjeiJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
